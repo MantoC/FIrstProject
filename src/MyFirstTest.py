620541ff3755aaa6d5aa873edb3722713f687b9b
chang <jane100992@gmail.com>
2017-03-16 15:07:42 +0800

    my first commit

---
+++ src/MyFirstTest.py
@@ -0,0 +1,9 @@
+username =  raw_input("please enter your name : ")
+
+age = input("please enter your age : ")
+
+gender = raw_input("please enter your gender M/F : ")
+
+if gender != 'F' or gender != 'M' : print ""
+
+
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-print 'Hello Mato'
+print 'Hello Manto'
 
 x = 100
 
@@ -9,3 +9,26 @@ if x ==  100 : print  'Hello Cenxui'
 if y is 'Cenxui' : print  'you are my lover'
 
 print  'i love you'
+
+username = raw_input("please enter your name : ")
+
+age = input('please enter your age : ')
+
+weight = input('please enter your weight :')
+
+height = input('please enter your height :')
+
+bmi = weight/ (height*height)
+
+print 'Hello ' + username + ', nice to meet you. \n'
+print  "Here is your BMI : %f "  % bmi
+
+if bmi >23 : print  "It's time to go on a diet."
+
+elif bmi <18 : print  "You need to gain more weight."
+
+else: print "You are in good shape."
+
+
+
+
